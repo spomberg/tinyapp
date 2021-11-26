2c8cf9f7256f0cb9d7f1b4c65b47ce02c29f0055
Marcos Spomberg <marcosspomberg@gmail.com>
2021-11-26 00:58:48 +0000

    Add new URL route and implement function to generate a random string

--- express_server.js
+++ express_server.js
@@ -1,13 +1,36 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 const app = express();
-const PORT = 8080; // default port 8080
+const PORT = 8080;
 
+app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+function generateRandomString() {
+  let result = "";
+
+  const generateOneChar = (n) => {
+    const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    if (n > 0) {
+      result += chars[Math.floor(Math.random() * 54)];
+      generateOneChar(n - 1);
+    }
+  }
+
+  generateOneChar(6);
+
+  return result;
+};
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+})
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -16,6 +39,10 @@ app.get("/", (req, res) => {
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
